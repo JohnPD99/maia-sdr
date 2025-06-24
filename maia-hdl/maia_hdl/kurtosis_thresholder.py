31107204cf9cfbb3158e9f0a29b877cc7519f47c
John <john.dornbierer@outlook.com>
2025-06-24 16:58:50 +0000

    added kurtosis thresholding module

---
+++ maia-hdl/maia_hdl/kurtosis_thresholder.py
@@ -0,0 +1,23 @@
+from amaranth import *
+import amaranth.cli
+
+
+class Kurtosis_Tresholder(Elaboratable):
+    """Skid buffer.
+
+    Parameters
+    ----------
+    
+
+    Attributes
+    ----------
+   
+    """
+    def __init__(self):
+        print("Todo")
+        
+    def elaborate(self, platform):
+        m = Module()
+
+
+        return m
